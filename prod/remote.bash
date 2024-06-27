e5fbd790fdba79f609292de0cedb72d9251ab813
crepusculumx <crepusculumx@qq.com>
2024-06-27 22:04:53 +0800

    chore: Update remote.bash.

--- prod/remote.bash
+++ prod/remote.bash
@@ -1,8 +1,11 @@
-ssh crepusculumx@121.36.210.113 "rm -rf /home/crepusculumx/server"
-scp -r ../prod crepusculumx@121.36.210.113:/home/crepusculumx/server/
+ssh crepusculumx@121.36.210.113 "rm -rf /home/crepusculumx/server;
+ mkdir /home/crepusculumx/server"
+
+scp -r ./ crepusculumx@121.36.210.113:/home/crepusculumx/server/
 
 # podman_run.prod.bash内部依赖工作目录
 ssh crepusculumx@121.36.210.113 "
 ln -s /home/crepusculumx/crepus-server-resources/blog /home/crepusculumx/server/resources/public ;
+ln -s /home/crepusculumx/crepus-server-resources/pomodoro /home/crepusculumx/server/resources/public ;
 cd /home/crepusculumx/server ;
 bash /home/crepusculumx/server/podman_run.prod.bash rework"
